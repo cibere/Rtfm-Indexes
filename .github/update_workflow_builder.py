ad0d00d06e556ccaee053db93196a7e1bb6ee048
cibere <71997063+cibere@users.noreply.github.com>
2025-02-08 20:46:38 -0800

    Update update_workflow_builder.py

--- .github/update_workflow_builder.py
+++ .github/update_workflow_builder.py
@@ -20,7 +20,7 @@ def iter_parser_files():
     for file in parsers_dir.glob("*.py"):
         if not file.name.startswith("_"):
             yield file
-
+ 
  
 def build_tree():
     files = [p.name.removesuffix(".py") for p in iter_parser_files()]
