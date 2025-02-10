f1fc8dc19d690db03fb42a4836c533c9bfbf1738
cibere <71997063+cibere@users.noreply.github.com>
2025-02-09 20:00:55 -0800

    add github docs parsers

---
+++ _parsers/docs.github.com.py
@@ -0,0 +1,21 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class GithubSearchAPI(
+    BaseAPI,
+    file=__file__,
+    url="https://docs.github.com/api/search/v1",
+    favicon_url="hthttps://github.com/favicon.ico",
+    api_type="Github",
+): ...
+
+
+GithubSearchAPI.build(__name__)
---
+++ _parsers/github.com.py
@@ -0,0 +1,10 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec",
+# ]
+# ///
+
+from _base import alias
+
+alias("docs.github.com", __file__)
