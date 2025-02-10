2d3fff6abbef1237a0adf9273a317705cbe27470
cibere <71997063+cibere@users.noreply.github.com>
2025-02-09 19:46:38 -0800

    Update _msgpack_to_json.py

--- _msgpack_to_json.py
+++ _msgpack_to_json.py
@@ -2,24 +2,48 @@
 # requires-python = ">=3.13"
 # dependencies = [
 #     "msgspec",
+#     "requests",
+#     "yarl",
 # ]
 # ///
-from pathlib import Path
-from msgspec import msgpack, json
 import sys
+from pathlib import Path
 
-args = sys.argv[1:]
+import requests
+from msgspec import json, msgpack
+from yarl import URL
 
-before = Path(args[0])
 
-try:
-    after = Path(args[1])
-except IndexError:
-    after = None
+def get_local_file(path: Path) -> bytes:
+    return path.read_bytes()
 
-data = msgpack.decode(before.read_bytes())
-pretty = json.format(json.encode(data)).decode()
-print(pretty)
 
-if after:
-    after.write_text(pretty)
+def get_web_file(url: URL) -> bytes:
+    return requests.get(str(url), timeout=10).content
+
+
+def main():
+    args = sys.argv[1:]
+    loc = args[0]
+
+    if loc.startswith(("http://", "https://")):
+        content = get_web_file(URL(loc))
+    else:
+        content = get_local_file(Path(loc))
+
+    try:
+        after = Path(args[1])
+    except IndexError:
+        after = None
+
+    data = msgpack.decode(content)
+    pretty = json.format(json.encode(data)).decode()
+    print(pretty)
+
+    if after:
+        after.write_text(pretty)
+        print(f"Wrote result to {after}")
+
+
+if __name__ == "__main__":
+    main()
