dbbdbf28f2e399378d2c4af9e8b466cf6376bfee
cibere <71997063+cibere@users.noreply.github.com>
2025-02-12 12:22:18 -0800

    Update update_workflow_builder.py

--- .github/update_workflow_builder.py
+++ .github/update_workflow_builder.py
@@ -62,8 +62,13 @@ def build_tree():
                         "continue-on-error": True,
                         "run": "\n".join(
                             [
+                                "git add indexes_2",
+                                "git stash",
+
                                 "git fetch",
                                 "git switch indexes-v2",
+
+                                "git stash pop 0",
                                 "git add indexes_2",
                                 'git -c user.name="github-actions[bot]" -c user.email="41898282+github-actions[bot]@users.noreply.github.com" commit --author="cibere <71997063+cibere@users.noreply.github.com>" -m "Auto Update Indexes"',
                                 "git push",
