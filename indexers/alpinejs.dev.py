fbbcad336d0efb888f6e50ffaf2f9f5ac8321156
cibere <71997063+cibere@users.noreply.github.com>
2025-02-28 12:54:04 -0800

    add alpinejs indexer

---
+++ indexers/alpinejs.dev.py
@@ -0,0 +1,50 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec==0.19.0",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class AlpineJsSearchAPI(
+    BaseAPI,
+    file=__file__,
+    api_type="algolia",
+    favicon_url="https://cdn.prod.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png",
+    options={
+        "url": "https://sm9gagaukz-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (5.19.0); Lite (5.19.0); Browser; docsearch (3.9.0); docsearch-react (3.9.0); docsearch.js (3.9.0)&x-algolia-api-key=1fad8740c0cf75209d11ae25f1f6f55c&x-algolia-application-id=SM9GAGAUKZ",
+        "payload": {
+            "indexName": "alpinejs",
+            "attributesToRetrieve": [
+                "hierarchy.lvl0",
+                "hierarchy.lvl1",
+                "hierarchy.lvl2",
+                "hierarchy.lvl3",
+                "hierarchy.lvl4",
+                "hierarchy.lvl5",
+                "hierarchy.lvl6",
+                "content",
+                "type",
+                "url",
+            ],
+            "attributesToSnippet": [
+                "hierarchy.lvl1:10",
+                "hierarchy.lvl2:10",
+                "hierarchy.lvl3:10",
+                "hierarchy.lvl4:10",
+                "hierarchy.lvl5:10",
+                "hierarchy.lvl6:10",
+                "content:10",
+            ],
+            "snippetEllipsisText": "…",
+            "hitsPerPage": 20,
+            "clickAnalytics": False,
+        },
+    },
+): ...
+
+
+AlpineJsSearchAPI.build(__name__)
