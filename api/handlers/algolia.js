a32c3e0ce2294eed8ffe85eeed1abf2caee6cf27
cibere <71997063+cibere@users.noreply.github.com>
2025-03-01 09:46:17 -0800

    update algolia api handler

--- api/handlers/algolia.js
+++ api/handlers/algolia.js
@@ -8,20 +8,42 @@ export async function algoliaHandler(requestInfo){
     }).then(v => v.json());
     const cache = {};
 
+    console.log("recieved response", data);
+
     for (let result of data.results){
         for (let hit of result.hits){
+            // console.log("dealing with hit", hit);
             const parts = [];
             for (let i = 0; i <= 6; i++) {
                 const lvl = `lvl${i}`;
-                if (hit.hierarchy[lvl] != null) {
-                    parts.push(hit.hierarchy[lvl]);
-                }
+                try {
+                    if (hit.hierarchy[lvl] != null) {
+                        parts.push(hit.hierarchy[lvl]);
+                    }
+                } catch {};
+            }
+            
+            let text = parts.join(" - ");
+            if (text == "") {
+                text = hit.title
             }
 
-            let text = parts.join(" - ");
+            let options = {}
+
+            let excerpt	= hit.excerpt;
+            if (excerpt	!= null) {
+                options["sub"] = excerpt
+            }
+
+            let href = hit.url
+            if (href == null) {
+                href = hit.slug
+            }
+            
             cache[text] = {
                 text,
-                url: hit.url
+                url: href,
+                options
             };
         };
     };
