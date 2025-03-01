38ecbcf6b2ad49bab311f7f7ddcca8a7d898bed7
cibere <71997063+cibere@users.noreply.github.com>
2025-03-01 12:18:41 -0800

    Update container.py

--- indexers/_base/container.py
+++ indexers/_base/container.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 
 import inspect
-from collections.abc import Sequence
 
 from .indexer import BaseIndexer
 
 
 class Container(BaseIndexer):
-    def _runner(self) -> None:
-        variants: Sequence[tuple[str, type[BaseIndexer]]] = inspect.getmembers(
-            self, lambda obj: issubclass(obj, BaseIndexer)
-        )
+    def get_members(self) -> dict[str, type[BaseIndexer]]:
+        return dict(inspect.getmembers(self, lambda obj: issubclass(obj, BaseIndexer)))
 
-        for name, cls in variants:
+    def _runner(self) -> None:
+        variants = self.get_members()
+
+        for name, cls in variants.items():
             cls._run_with_variant(name)
