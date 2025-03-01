18d99cc982f8fa12776123042b6efe1e52be1c2b
cibere <71997063+cibere@users.noreply.github.com>
2025-02-28 20:06:28 -0800

    update the base api url builder

--- indexers/_base/api.py
+++ indexers/_base/api.py
@@ -1,9 +1,16 @@
 from __future__ import annotations
 
+import sys
 from typing import Any, ClassVar
 
 from .indexer import BaseIndexer
 from .structs import ApiIndex
+
+BASE_URL = (
+    "http://127.0.0.1:8787"
+    if sys.argv[-1] == "--debug"
+    else "https://rtfm-index-api.cibere.dev"
+)
 
 
 class BaseAPI(BaseIndexer):
@@ -15,7 +22,7 @@ class BaseAPI(BaseIndexer):
             ApiIndex(
                 self.name,
                 self.favicon_url,
-                f"https://rtfm-index-api.cibere.dev/{self.api_type}",
+                f"{BASE_URL}/{self.api_type}",
                 self.options,
             ),
             self.name,
