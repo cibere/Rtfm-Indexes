cc04c9d825ff913106d04d311d2193c4cce2c5d1
cibere <71997063+cibere@users.noreply.github.com>
2025-03-01 09:56:44 -0800

    add babeljs indexer

---
+++ indexers/babeljs.io.py
@@ -0,0 +1,51 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec==0.19.0",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class BabeljsSearchAPI(
+    BaseAPI,
+    file=__file__,
+    api_type="algolia",
+    favicon_url="https://babeljs.io/img/favicon.png",
+    options={
+        "url": "https://m7kgjdk6wf-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (5.19.0); Lite (5.19.0); Browser; docsearch (3.9.0); docsearch-react (3.9.0); docusaurus (3.7.0)&x-algolia-api-key=6ec7d6acbfb6ed3520846a7517533c28&x-algolia-application-id=M7KGJDK6WF",
+        "payload": {
+            "indexName": "babeljs",
+            "attributesToRetrieve": [
+                "hierarchy.lvl0",
+                "hierarchy.lvl1",
+                "hierarchy.lvl2",
+                "hierarchy.lvl3",
+                "hierarchy.lvl4",
+                "hierarchy.lvl5",
+                "hierarchy.lvl6",
+                "content",
+                "type",
+                "url",
+            ],
+            "attributesToSnippet": [
+                "hierarchy.lvl1:10",
+                "hierarchy.lvl2:10",
+                "hierarchy.lvl3:10",
+                "hierarchy.lvl4:10",
+                "hierarchy.lvl5:10",
+                "hierarchy.lvl6:10",
+                "content:10",
+            ],
+            "snippetEllipsisText": "…",
+            "hitsPerPage": 20,
+            "clickAnalytics": False,
+            "facetFilters": [],
+        },
+    },
+): ...
+
+
+BabeljsSearchAPI.build(__name__)
