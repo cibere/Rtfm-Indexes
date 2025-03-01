0ac20ddfc1f231c7efe32525f56f6f4db52688fb
cibere <71997063+cibere@users.noreply.github.com>
2025-03-01 15:26:49 -0800

    update Containers

--- indexers/_base/__init__.py
+++ indexers/_base/__init__.py
@@ -2,6 +2,7 @@ from ._types import Cache as Cache
 from ._types import MutableCache as MutableCache
 from .api import BaseAPI as BaseAPI
 from .container import Container as Container
+from .container import ContainerMembers as ContainerMembers
 from .parser import BaseAsyncParser as BaseAsyncParser
 from .parser import BaseSyncParser as BaseSyncParser
 from .structs import Entry as Entry
--- indexers/_base/container.py
+++ indexers/_base/container.py
@@ -1,16 +1,25 @@
 from __future__ import annotations
 
+import asyncio
+from collections.abc import Coroutine
+from inspect import isawaitable
+from typing import Any
+
 from .indexer import BaseIndexer
+
+type ContainerMembers = dict[str, tuple[type[BaseIndexer], tuple[str, ...]]]
 
 
 class Container(BaseIndexer):
-    def get_members(self) -> dict[str, tuple[type[BaseIndexer], tuple[str, ...]]]:
+    def get_members(self) -> ContainerMembers | Coroutine[Any, Any, ContainerMembers]:
         """{variant:(cls, [subvariants])}"""
 
         raise NotImplementedError
 
     def _runner(self) -> None:
         variants = self.get_members()
+        if isawaitable(variants):
+            variants = asyncio.run(variants)
 
         for name, (cls, raw_subvariants) in variants.items():
             subvariants = [f"{name}-{subvar}" for subvar in raw_subvariants]
