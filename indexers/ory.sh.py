880764e898529a9d6c6f2c88fa3720dd41f3020f
cibere <71997063+cibere@users.noreply.github.com>
2025-03-03 12:39:19 -0800

    add an indexer for ory.sh

---
+++ indexers/ory.sh.py
@@ -0,0 +1,54 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec==0.19.0",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class SearchAPI(
+    BaseAPI,
+    file=__file__,
+    api_type="algolia",
+    favicon_url="https://www.ory.sh/docs/img/favico.png",
+    options={
+        "url": "https://v2efiwej25-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (5.14.2); Lite (5.14.2); Browser; docsearch (3.8.2); docsearch-react (3.8.2); docusaurus (3.7.0)&x-algolia-api-key=dc6b220f7d2bcd12da60b9cce431d8c5&x-algolia-application-id=V2EFIWEJ25",
+        "payload": {
+            "indexName": "ory",
+            "attributesToRetrieve": [
+                "hierarchy.lvl0",
+                "hierarchy.lvl1",
+                "hierarchy.lvl2",
+                "hierarchy.lvl3",
+                "hierarchy.lvl4",
+                "hierarchy.lvl5",
+                "hierarchy.lvl6",
+                "content",
+                "type",
+                "url",
+            ],
+            "attributesToSnippet": [
+                "hierarchy.lvl1:10",
+                "hierarchy.lvl2:10",
+                "hierarchy.lvl3:10",
+                "hierarchy.lvl4:10",
+                "hierarchy.lvl5:10",
+                "hierarchy.lvl6:10",
+                "content:10",
+            ],
+            "snippetEllipsisText": "…",
+            "hitsPerPage": 20,
+            "clickAnalytics": False,
+            "facetFilters": [
+                "language:en",
+                ["docusaurus_tag:default", "docusaurus_tag:docs-default-current"],
+            ],
+        },
+    },
+): ...
+
+
+SearchAPI.build(__name__)
