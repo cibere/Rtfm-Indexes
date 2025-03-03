7497ce3ac9dc771a3e28a4bf110baa229e7dec40
cibere <71997063+cibere@users.noreply.github.com>
2025-03-02 16:32:08 -0800

    add an indexer for docs.insomnia.rest

---
+++ indexers/docs.insomnia.rest.py
@@ -0,0 +1,24 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec==0.19.0",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class InsomniaRestSearchAPI(
+    BaseAPI,
+    file=__file__,
+    api_type="algolia",
+    # favicon_url="",
+    options={
+        "url": "https://ibxe7r5kfs-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for vanilla JavaScript (lite) 3.30.0;docsearch.js 2.6.3&x-algolia-application-id=IBXE7R5KFS&x-algolia-api-key=65f451796f36a6d7df9814965f5de7ac",
+        "payload": {"indexName": "insomnia_docs", "hitsPerPage": 8},
+    },
+): ...
+
+
+InsomniaRestSearchAPI.build(__name__)
