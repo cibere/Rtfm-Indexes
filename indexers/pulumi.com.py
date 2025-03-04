517ca5f2c4db4322e99f861c8e8edbdf14e6d3c0
cibere <71997063+cibere@users.noreply.github.com>
2025-03-04 11:55:43 -0800

    add an indexer for pulumi.com

---
+++ indexers/pulumi.com.py
@@ -0,0 +1,30 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "msgspec==0.19.0",
+# ]
+# ///
+from __future__ import annotations
+
+from _base import BaseAPI
+
+
+class SearchAPI(
+    BaseAPI,
+    file=__file__,
+    api_type="algolia",
+    # favicon_url="",
+    options={
+        "url": "https://p6kicoku8e-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia for JavaScript (4.17.0); Browser (lite); autocomplete-core (1.9.2); autocomplete-js (1.9.2)&x-algolia-api-key=9ac9f1177f81bc8cb5f6cf30485e62db&x-algolia-application-id=P6KICOKU8E",
+        "payload": {
+            "indexName": "production",
+            "hitsPerPage": 50,
+            "clickAnalytics": False,
+            "filters": '(section:"Docs" OR section:"Registry" OR section:"Tutorials")',
+        },
+        "url_template": "https://www.pulumi.com{href}",
+    },
+): ...
+
+
+SearchAPI.build(__name__)
